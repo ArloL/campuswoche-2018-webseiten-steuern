70ae32500d4980fc6655fa199aa80062de43b245
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2018-07-23 17:51:55 +0200

    Add sample for mittagstisch

--- src/main/java/de/campuswoche/browser/BrowserStart.java
+++ src/main/java/de/campuswoche/browser/BrowserStart.java
@@ -1,12 +1,13 @@
 package de.campuswoche.browser;
 
-import static java.util.stream.Collectors.toList;
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrowserStart implements ApplicationRunner {
 
-	private static final Logger LOG =
-	        LoggerFactory.getLogger(BrowserStart.class);
+	private static final Logger LOG = LoggerFactory.getLogger(BrowserStart.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrowserStart.class, args);
@@ -27,22 +27,43 @@ public class BrowserStart implements ApplicationRunner {
 
 	@Autowired
 	WebDriver browser;
+	@Autowired
+	WebDriverWait webDriverWait;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		beiMilla();
 
-		browser.get("http://www.youtube.com");
-
-		List<WebElement> elements = browser.findElements(By.cssSelector("a"));
-
-		List<String> links = elements.stream()
-		    .filter(e -> e.getText() != null && !e.getText().isEmpty())
-		    .map(WebElement::getText).collect(toList());
-
-		LOG.info("{}", links);
+		truckstopBocholt();
 
 		browser.quit();
+	}
 
+	private void beiMilla() {
+		browser.get("https://cityhotel-bocholt.de/wochen-mittagskarte/");
+
+		WebElement milla = webDriverWait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.aligncenter.size-large")));
+
+		LOG.info("Milla Links: {}", milla.getAttribute("src"));
+	}
+
+	private void truckstopBocholt() {
+		browser.get("http://truckstop-bocholt.de/");
+
+		WebElement mittagstisch = webDriverWait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Mittagstisch")));
+
+		String link = mittagstisch.getAttribute("href");
+
+		browser.get(link);
+
+		browser.switchTo().frame("CustomTappIframe");
+
+		List<WebElement> elements = webDriverWait
+				.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("iframe")));
+
+		LOG.info("TruckStop Links: {}", elements.stream().map(e -> e.getAttribute("src")).collect(Collectors.toList()));
 	}
 
 }
