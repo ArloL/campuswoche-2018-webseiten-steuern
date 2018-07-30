e559fad19056f341b5a442d2aee34640c7d9ae0d
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2018-07-30 15:33:20 +0200

    Update Http stuff

---
+++ src/main/java/de/campuswoche/http/HttpBase.java
@@ -0,0 +1,33 @@
+package de.campuswoche.http;
+
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+public class HttpBase {
+
+	@Autowired
+	Environment environment;
+	Document document;
+	
+	public void openWebsite(String url) throws IOException {
+		document = connect(url).get();
+	}
+
+	public Connection connect(String url) {
+		Connection connect = Jsoup.connect(url);
+
+		String host = environment.getProperty("jbd.proxyhost");
+		Integer port = environment.getProperty("jbd.proxyport", Integer.class);
+		if (host != null && port != null) {
+			return connect.proxy(host, port);
+		}
+
+		return connect;
+	}
+
+}
--- src/main/java/de/campuswoche/http/HttpStart.java
+++ src/main/java/de/campuswoche/http/HttpStart.java
@@ -1,13 +1,5 @@
 package de.campuswoche.http;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.List;
-
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HttpStart implements ApplicationRunner {
+public class HttpStart extends HttpBase implements ApplicationRunner {
 
 	private static final Logger LOG = LoggerFactory.getLogger(HttpStart.class);
 
@@ -27,20 +19,11 @@ public class HttpStart implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-
-		Document document = connect("https://www.youtube.com").get();
-
-		Elements elements = document.select("a");
-
-		List<String> links = elements.stream()
-		    .filter(e -> e.text() != null && !e.text().isEmpty())
-		    .map(Element::text).collect(toList());
+		openWebsite("https://www.kinodrom.de/programm");
+		
+		Elements links = document.select("a");
 
 		LOG.info("{}", links);
-	}
-
-	Connection connect(String url) {
-		return Jsoup.connect(url).proxy("proxy.herbrand.rz.evosec.de", 8080);
 	}
 
 }
